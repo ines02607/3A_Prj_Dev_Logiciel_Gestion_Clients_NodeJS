ae8967a6b15a6e255a9a95d5ae9518e5335e9ba8
ines02607 <lina.mazouz@free.fr>
2023-03-25 10:44:56 +0100

    essaie nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 // on va importer le package http de node pour créer un serveur
-/*const http = require('http');
+const http = require('http');
 // à chaque envoie au serveur, on va appeler
 const server = http.createServer((req, res) => {
-    res.end('Voila la réponse du serveur ! ');
+    res.end('Voila la réponse du serveur bientot disponible  ! ');
 });
 
-server.listen(process.env.PORT || 4000);*/
+server.listen(process.env.PORT || 3000);
