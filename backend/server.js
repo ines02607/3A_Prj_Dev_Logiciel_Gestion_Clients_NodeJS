e585f0484c46cf28cebc209c4398a6ed9188cec1
ines02607 <lina.mazouz@free.fr>
2023-03-25 09:49:44 +0100

    dev server js

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+// on va importer le package http de node pour créer un serveur
+const http = require('http');
+// à chaque envoie au serveur, on va appeler
+const server = http.createServer((req, res) => {
+    res.end('Voila la réponse du serveur ! ');
+});
+
+server.listen(process.env.PORT || 3000);
