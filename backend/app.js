28930a519ef6623edbd962d15095230e54d36608
ines02607 <lina.mazouz@free.fr>
2023-03-25 15:05:17 +0100

    lien entre front et back

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
-
 app.use(express.static("../Front/public"));
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
