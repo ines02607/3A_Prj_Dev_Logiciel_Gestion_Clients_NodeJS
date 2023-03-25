b3b19be7325640e1fa19866ec6506143d8b277ee
ines02607 <lina.mazouz@free.fr>
2023-03-25 10:20:39 +0100

    changement essaie pagination

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 // on va importer le package http de node pour créer un serveur
-const http = require('http');
+/*const http = require('http');
 // à chaque envoie au serveur, on va appeler
 const server = http.createServer((req, res) => {
     res.end('Voila la réponse du serveur ! ');
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 4000);*/
