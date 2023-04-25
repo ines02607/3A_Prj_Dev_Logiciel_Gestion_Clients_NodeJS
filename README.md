4df3694f04c175e7ea2ea8e3297570bbc27f2bbe
ines02607 <lina.mazouz@free.fr>
2023-04-26 01:33:16 +0200

    esthetique

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 ## Lancement du projet
 
-J'ai installé nodemon pour le lancement du serveur en continue
+J'ai installé "nodemon" pour le lancement du serveur en continue
 
 > $ nodemon main.js
 
 Puis, dans le navigateur, il faut lancer :
 
 > http://localhost:3000
+
+J'ai également début une initialisation sous mongoDB sans succès (partie commenté dans le fichier main.js)
+
+## Attention : Parfois, il faut rafraichir la page pour accéder aux données actualisées !
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  last: { type: String, required: true },
-  first: { type: String, required: true },
-  email: { type: String, required: true },
-  company: { type: String, required: true },
-  country: {type: String, required:true},
+    last: { type: String, required: true },
+    first: { type: String, required: true },
+    email: { type: String, required: true },
+    company: { type: String, required: true },
+    country: {type: String, required:true},
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
